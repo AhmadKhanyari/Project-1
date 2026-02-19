cce214d09eff23f54f9f2a97fccd5aa06db63b15
AhmadKhanyari <ahmedkhanyari787@gmail.com>
2026-02-19 16:43:58 +0530

    changes added

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
 <body>
     <div class = "container">
         This My First Php Webpage!
+        <?php
+            echo "Hello World!";
+
+        //This is a single line comment
+        /* This is a multi-line comment
+        This is a multi-line comment*/
+
+//Lorem ipsum dolor sit amet consectetur.
+
+     $var1 = 50;
+     $var2 = 36;
+        $sum = $var1 + $var2;
+        echo "The sum of $var1 and $var2 is: $sum";
+        ?>
     </div>
 </body>
 </html>
